bca088a1d6fe6ad4d7a0aefc5c8b9c2476a6ea0b
Ghalileo <ose.okogbo@gmail.com>
2022-01-18 22:45:25 -0600

    Details page now rendering as intended

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { forkJoin, Observable } from 'rxjs';
+import {map} from 'rxjs/operators'
 import { environment as env } from 'src/environments/environment'
 import { APIResponse, Game } from '../models';
 
@@ -17,11 +18,35 @@ export class HttpService {
     let params = new HttpParams().set('ordering', ordering);
 
     if(search) {
-      params = new HttpParams().set('ordering', ordering);
+      params = new HttpParams().set('ordering', ordering).set('search', search);
     }
 
     return this.http.get<APIResponse<Game>>(`${env.BASE_URL}/games`, {
       params:params,
     })
   }
+
+  getGameDetails(id: string): Observable<Game> {
+    const gameInfoRequest = this.http.get(`${env.BASE_URL}/games/${id}`);
+    const gameTrailerRequest = this.http.get(
+      `${env.BASE_URL}/games/${id}/movies`
+    );
+    const gameScreenshotsReqest = this.http.get(
+      `${env.BASE_URL}/games/${id}/screenshots`
+    );
+    return forkJoin({
+      gameInfoRequest,
+      gameScreenshotsReqest,
+      gameTrailerRequest
+    }).pipe(
+      map((resp: any)=>{
+        return{
+          ...resp['gameInfoRequest'],
+          screenshots: resp['gameScreenshotsRequest']?.results,
+          trailers: resp['gameTrailersRequest']?.results,
+        };
+      })
+    );
+  }
+
 }
